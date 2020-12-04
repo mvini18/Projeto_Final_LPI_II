f38dfdf458d2af98effe990db5b3dd8d209032dd
Marcos Coelho <marcosv2001@outlook.com>
2020-12-03 22:51:05 -0300

    antigo marcos

--- ProjetoAtestado/WebContent/js/senha_registro.js
+++ ProjetoAtestado/WebContent/js/senha_registro.js
@@ -1,9 +1,10 @@
-function validatePassword(){
+function validatePassword() {
 	var password = document.getElementById("senha"), confirm_password = document.getElementById("confirmar_senha");
 	if(password.value != confirm_password.value) {
-		setCustomValidity("Senhas diferentes!");
+		confirm_password.setCustomValidity("Senhas diferentes!");
 		return false;
 	} else {
+		confirm_password.setCustomValidity('');
 		return true;
 	}
 }
